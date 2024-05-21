9413256c8a515d6ad9dd3e6b07bdbf4cbc64b6fd
rmdhnz <ramadhanfadhly2@gmail.com>
2024-05-21 08:09:12 +0700

    menambah filter wiener

--- mynoise.py
+++ mynoise.py
@@ -1,17 +1,22 @@
 import cv2 as cv
-# import numpy as np
+from scipy import signal,ndimage
 from skimage.util import random_noise
 from skimage import io
 class NoiseForImage : 
-
   def __init__(self,image_location) : 
     self.image = cv.imread(image_location)
   def resize_image(self,width,height) : 
     self.image=cv.resize(self.image,(width,height))
   def grayscale_image(self) : 
     self.image=cv.cvtColor(self.image,cv.COLOR_BGR2GRAY)
+
+  def wiener_filter(self,width=None,height=None) : 
+    if width==None or height==None : 
+      self.image=signal.wiener(self.image)
+    else :
+      self.image=signal.wiener(self.image,(width,height))
   
-  def display(self,title) : 
+  def display(self,title="document") : 
     cv.imshow(title,self.image)
   
   def gaussian_noise(self,mean,var) :
